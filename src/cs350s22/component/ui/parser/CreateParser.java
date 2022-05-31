52d9c2a924fbcdf3ab7211a52a88965255aa156a
Ocean Oestreicher <ooestreicher@ewu.edu>
2022-05-31 14:08:24 -0700

    Reporter Parse Comments

--- src/cs350s22/component/ui/parser/CreateParser.java
+++ src/cs350s22/component/ui/parser/CreateParser.java
@@ -14,7 +14,12 @@ public class CreateParser {
     }
 
     protected static void reporterParse(final A_ParserHelper ph, final Command cmd) {
-
+        //CREATE REPORTER CHANGE id NOTIFY [ids] [groups] DELTA value
+        //CREATE REPORTER FREQUENCY id NOTIFY [ids] [groups] FREQUENCY value
+        //ReporterFrequency(List<Identifier> ids, List<Identifier> groups, int reportingFrequency)
+        //ReporterChange(List<Identifier> ids, List<Identifier> groups, int deltaThreshold)
+        //Create a new ReporterChange object with ids, groups, and value and add it to SymbolTable<A_Reporter>
+        //Create a new ReporterFrequency object with ids, groups, and value and add it to SymbolTable<A_Reporter>
     }
 
     protected static void sensorParse(final A_ParserHelper ph, final Command cmd) {
