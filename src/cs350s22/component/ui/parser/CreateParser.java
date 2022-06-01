012f6780abf4afafba711aa6ef839d362c0befdf
Ocean Oestreicher <ooestreicher.ewu.edu>
2022-06-01 13:51:51 -0700

    reporterParse refactor additional test added to actuatortester

--- src/cs350s22/component/ui/parser/CreateParser.java
+++ src/cs350s22/component/ui/parser/CreateParser.java
@@ -10,6 +10,7 @@ import cs350s22.support.Identifier;
 import cs350s22.test.ActuatorPrototype;
 import cs350s22.test.MySensor;
 
+import java.lang.reflect.Array;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -138,59 +139,45 @@ public class CreateParser {
     }
 
     protected static void reporterParse(final A_ParserHelper ph, final Command cmd) {
-        LinkedList<Identifier> ids = new LinkedList<>(), groups = new LinkedList<>();
-        boolean isChange = false, isFrequency = false, notify = false, hasIds = false, hasGroups = false, delta = false, frequency = false, isValid = false;
-        String ID = "";
-        int deltaFrequencyValue = 0;
-
-        while (cmd.hasNext() && !isValid) {
+        boolean  isChange = false, isFrequency = false;
+        List<Identifier>ids=null,groups=null;
+        Identifier ID;
+        int deltaOrFrequencyValue = 0;
+        try{
             String token = cmd.getNext();
+            //Change/Frequency
+            if(token.equalsIgnoreCase("CHANGE"))isChange = true;
+            else if(token.equalsIgnoreCase("FREQUENCY")) isFrequency = true;
+            else throw new RuntimeException("Invalid Reporter Argument");
+            //ID
+            ID = Identifier.make(cmd.getNext());
 
-            if (!isChange && !isFrequency) {
-                if (token.equalsIgnoreCase("change")) isChange = true;
-                else if (token.equalsIgnoreCase("frequency")) isFrequency = true;
-            }
-            else if (ID.equals("")) ID = token;
-            else if (!notify && token.equalsIgnoreCase("notify")) notify = true;
-            else if (token.toUpperCase().matches("GROUPS?|IDS?")) {
-                if (cmd.hasNext()) {
-                    if (token.equalsIgnoreCase("id") || token.equalsIgnoreCase("ids")) {
-                        hasIds = true;
-                        hasGroups = false;
-                    } else {
-                        hasGroups = true;
-                        hasIds = false;
-                    }
-                }
-                while (cmd.hasNext() && !cmd.equalsNext(new String[]{"id", "ids", "groups", "group", "delta", "frequency"})) {
-                    token = cmd.getNext();
-                    if (token.contains("\"")) throw new RuntimeException("Invalid Reporter Command");
-                    if (hasIds) ids.add(Identifier.make(token));
-                    else if (hasGroups) groups.add(Identifier.make(token));
+            if(!cmd.getNext().equalsIgnoreCase("NOTIFY"))throw new RuntimeException("Invalid Reporter Argument");
 
-                }
+            token = cmd.getNext();
+            if(token.toUpperCase().matches("IDS?")){
+                ids= getIdentifiers(cmd.collateTo(new String[]{"GROUP","GROUPS","DELTA","FREQUENCY"}));
             }
-            else if ((isChange && token.equalsIgnoreCase("delta")) || (isFrequency && token.equalsIgnoreCase("frequency"))) {
-                if (cmd.hasNext()) {
-                    token = cmd.getNext();
-                    if (cmd.isNumeric(token) && isChange) delta = true;
-                    else if (cmd.isNumeric(token) && isFrequency) frequency = true;
-                    deltaFrequencyValue = Integer.parseInt(token);
-                }
+            if(!(ids ==null)) token= cmd.getNext();
+            else ids =new LinkedList<>();
+            if(token.toUpperCase().matches("GROUPS?")){
+                groups = getIdentifiers(cmd.collateTo(new String[]{"DELTA","FREQUENCY"}));
             }
-            //Bad Syntax
-            else break;
-            if ((isChange || isFrequency) && !ID.equals("") && notify && (hasIds || hasGroups) && (delta || frequency))
-                isValid = true;
+            if(!(groups == null))token = cmd.getNext();
+            else groups = new LinkedList<>();
+            if(token.equalsIgnoreCase("delta")||token.equalsIgnoreCase("frequency")){
+                deltaOrFrequencyValue = Integer.parseInt(cmd.getNext());
+            }
+            if(cmd.hasNext())throw new RuntimeException("Invalid Reporter Command Argument Count");
+
+            A_Reporter theReporter = null;
+            if(isChange) theReporter = new ReporterChange(ids, groups, deltaOrFrequencyValue);
+            else theReporter = new ReporterFrequency(ids, groups, deltaOrFrequencyValue);
+            ph.getSymbolTableReporter().add(ID,theReporter);
+
         }
-        if (!isValid || cmd.hasNext()) throw new RuntimeException("Invalid Reporter Command");
-
-        if (isChange) {
-            ReporterChange reporter = new ReporterChange(ids, groups, deltaFrequencyValue);
-            ph.getSymbolTableReporter().add(Identifier.make(ID), reporter);
-        } else {
-            ReporterFrequency reporter = new ReporterFrequency(ids, groups, deltaFrequencyValue);
-            ph.getSymbolTableReporter().add(Identifier.make(ID), reporter);
+        catch(ArrayIndexOutOfBoundsException|NumberFormatException e){
+            throw new RuntimeException("Invalid Reporter Command Entered");
         }
     }
 
