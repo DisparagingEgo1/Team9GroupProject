0cc25f708952acb1994fc12964b24cc5260c7c3d
Clockknight <tylerf2wang@gmail.com>
2022-06-03 23:45:32 -0700

    Partial progress on sendparser, passes all test where it should throw exceptions

--- src/cs350s22/component/ui/parser/SendParser.java
+++ src/cs350s22/component/ui/parser/SendParser.java
@@ -1,38 +1,45 @@
 package cs350s22.component.ui.parser;
 
+import java.util.Arrays;
+
 public class SendParser {
-    protected static void sendParse(final A_ParserHelper ph, final Command cmd){
+    protected static void sendParse(final A_ParserHelper ph, final Command cmd) {
         String[] ids = new String[cmd.length()];
         String[] groups = new String[cmd.length()];
-        String cur;
+        int idindex = 0;
+        int groupindex = 0;
+        String cur = "";
         boolean examid = false;
         boolean examgroup = false;
-        if(cmd.length() < 3)
-        {
+        if (cmd.length() < 3) {
             throw new RuntimeException("Bad Params passed to sendParse");
         }
-        while(cmd.hasNext())
-        {
+        while (cmd.hasNext()) {
+
             cur = cmd.getNext().toUpperCase();
 
             // failure to specify id or group after asking
             // need to find way to specify to only go into fail check when the thing we're examining is empty
-            if (examid || examgroup)
-            {
-                if ((cur.equals("ID") || cur.equals("IDS") || cur.equals("GROUPS") || cur.equals("GROUP") || cur.equals("PING") || cur.equals("POSITION"))){
+            if ((examid && ids[0] == null) || (examgroup && groups[0] == null) ) {
+                if (cur.equals("PING") || cur.equals("POSITION")) {
+                    throw new RuntimeException("");
+                }
+                if ((cur.equals("ID") || cur.equals("IDS"))) {
+                    throw new RuntimeException("");
+                }
+                if ((cur.equals("GROUPS") || cur.equals("GROUP"))) {
                     throw new RuntimeException("");
                 }
             }
 
+
             //double quotes
-            if (cur.charAt(0) == '\"' || cur.charAt(cur.length()-1) == '\"')
-            {
+            if (cur.charAt(0) == '\"' || cur.charAt(cur.length() - 1) == '\"') {
                 throw new RuntimeException();
             }
 
             //switch for changing bools to check for id or group
-            switch (cur)
-            {
+            switch (cur) {
                 case "ID":
                     examid = true;
                     examgroup = false;
@@ -55,23 +62,32 @@ public class SendParser {
                     break;
                 case "POSITION":
                     // each cmd walks through what should be the next argument
-                    if(cmd.getNext().toUpperCase() != "REQUEST" || cmd.getNext().toUpperCase() != "30" || cmd.hasNext()){
+                    if (!examid && !examgroup) throw new RuntimeException();
+                    try {
+                        if (!cmd.getNext().equalsIgnoreCase("REQUEST")) {
+                            throw new RuntimeException();
+                        } else if (-1003 == Integer.parseInt(cmd.getNext()) || cmd.hasNext()) {
+                            throw new RuntimeException();
+                        }
+                    } catch (NumberFormatException e) {
                         throw new RuntimeException();
                     }
+
                     break;
+                case "REPORT":
+                    throw new RuntimeException();
                 default:
-                    if(examgroup)
-                    {
-                        groups[cmd.getTokenIndex()] = cur;
+                    if (examgroup) {
+                        groups[groupindex] = cmd.getCurrentToken();
+                        groupindex++;
                     } else if (examid) {
-                        ids[cmd.getTokenIndex()] = cur;
+                        ids[idindex] = cmd.getCurrentToken();
+                        idindex++;
                     } else {
                         throw new RuntimeException();
                     }
 
             }
         }
-
-
     }
 }
